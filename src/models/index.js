94b610ab6847b0bc258786e8266dd25091528198
ayadeleke <a.adeleke@alustudent.com>
2025-04-03 20:13:46 +0200

    clean_up

--- src/models/index.js
+++ src/models/index.js
@@ -1,16 +1,39 @@
+'use strict';
 require('dotenv').config(); // Load environment variables
-const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const { Sequelize, DataTypes } = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-// Use local PostgreSQL configuration
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: console.log,  // Optional: Enable logging of SQL queries
+// Initialize Sequelize
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(process.env.DATABASE_URL || config.database, config.username, config.password, config);
+}
+
+// Read and import all models
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js' && file.indexOf('.test.js') === -1)
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
+    db[model.name] = model;
+  });
+
+// Associate models if applicable
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-const User = require('./user'); // Import models
-// const Event = require('./events');
-
 // Sync models (use migrations in production)
-sequelize.sync() // Sync models (recommended to use migrations in production)
+sequelize.sync();
 
-module.exports = { sequelize, User, Event };
+// Export database connection and models
+module.exports = { sequelize, Sequelize, db };
